570f7e588c625a1c5d8b6aa252def08d3a999382
Saleelvk <87797952+Saleelvk@users.noreply.github.com>
2024-12-14 08:45:18 +0530

    Update Mainmenu.jsx

--- admin/src/Pages/Mainmenu.jsx
+++ admin/src/Pages/Mainmenu.jsx
@@ -52,7 +52,7 @@ function Menumenu() {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-900 text-white mt-14">
+    <div className="flex justify-center items-center min-h-screen bg-gray-900 text-white mt-20">
       <div className="w-full max-w-5xl p-6">
         <h2 className="text-3xl font-bold text-center mb-6 text-blue-500">Menu List</h2>
 
