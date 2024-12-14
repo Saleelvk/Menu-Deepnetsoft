be642d01480da67d623fe3eb39fdaffade579b4f
Saleelvk <87797952+Saleelvk@users.noreply.github.com>
2024-12-14 08:55:44 +0530

    Update Hero.jsx

--- admin/src/Components/Hero.jsx
+++ admin/src/Components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
   return (
     <div className="h-[100vh] bg-black flex items-center justify-center gap-5">
         <Link to={'/menu:/id'}>
-      <p className="text-white md:text-4xl  text-sm   bg-gray-900   px-10 py-5 rounded-lg hover:bg-blue-500 font-bold transition-all ease-in"> Add Menu </p>
+      <p className="text-white md:text-4xl  text-sm   bg-gray-900  px-6  md:px-10 md:py-5 py-3 rounded-lg hover:bg-blue-500 font-bold transition-all ease-in"> Add Menu </p>
         </Link>
         <Link to={'/menuItems'}>
       <p className="text-white md:text-4xl  text-sm   bg-gray-900  hover:bg-blue-500 font-bold transition-all ease-in  px-10 py-5 rounded-lg ">Add Menu items</p>
